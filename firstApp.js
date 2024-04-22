fff0dc5b83734d32de66075ffa057520b42a899c
Sandesh Gautam <sandesh.gautam9999@gmail.com>
2024-04-22 15:56:51 +0545

    pug view engine

--- firstApp.js
+++ firstApp.js
@@ -1,18 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
 const utils = require("./routes");
-const adminRoute = require("./router/admin");
+const adminData = require("./router/admin");
 const shopRoute = require("./router/shop");
+
+app.set("view engine", "pug");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/admin", adminRoute);
+app.use("/admin", adminData.routes);
 
 app.use(shopRoute);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Error 404: page not found!!!</h1>");
+  res.render("404", { title: "Error 404" });
 });
 
 app.listen(3000);
--- router/shop.js
+++ router/shop.js
@@ -2,8 +2,12 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 
+const adminData = require("./admin");
+
+const products = adminData.products;
+
 router.get("/", (req, res, next) => {
-  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+  res.render("shop", { prods: products, title: "shop" });
 });
 
 module.exports = router;
