c95cec5e16345af31476ebf9954404dbd45d9a74
Sandesh Gautam <sandesh.gautam9999@gmail.com>
2024-05-01 22:55:52 +0545

    new navigations added

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,6 +16,10 @@ exports.getCart = (req, res, next) => {
   res.render("shop/cart", { pageTitle: "Cart" });
 };
 
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", { pageTitle: "Your Order" });
+};
+
 exports.getCheckOut = (req, res, next) => {
   res.render("shop/checkout", { pageTitle: "Checkout" });
 };
